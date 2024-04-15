6fc2538d8f814e2b7624f947570dc546f3dbdf13
Ana Luiza <analuiza222r@gmail.com>
2024-04-15 19:41:59 -0300

    atualização horas e segundo

--- assets/java.js
+++ assets/java.js
@@ -1,6 +1,6 @@
 const horas = document.getElementById ('horas');
 const minutos = document.getElementById ('minutos');
-const segundos= document.getElementById ('horas');
+const segundos= document.getElementById ('segundos');
 
 const relogio = setInterval(function time(){
     let dateToday = new Date();
